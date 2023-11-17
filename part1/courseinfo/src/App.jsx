ad4bd4fb8538cee6ae1188a36324c7203f2a9303
Karel R. Varona <kwaref@gmail.com>
2023-11-17 16:27:59 +0000

    feat: 1.2 done

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,24 +1,20 @@
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part, exercise }) => <p>{`${part} ${exercise}`}</p>
 
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercise={exercises1}/> 
+      <Part part={part2} exercise={exercises2}/>
+      <Part part={part3} exercise={exercises3}/>
     </>
-  );
-};
+  )
+}
 
 const Total = ({exercises1, exercises2, exercises3}) => {
   return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-};
+}
 
 const App = () => {
   const course = "Half Stack application development"
