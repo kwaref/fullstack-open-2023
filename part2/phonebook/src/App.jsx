3cc0164f5a699a8b426b6dc4d6eb48343ca930fb
Karel R. Varona <kwaref@gmail.com>
2023-11-18 13:14:15 +0000

    feat: 2.9 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number:'040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const notIn = (str) => {
     const names = persons.map( person => person.name.toLowerCase())
@@ -15,7 +20,7 @@ const App = () => {
   const handleSubmit = (evt) => {
     evt.preventDefault()
     if (notIn(newName)) {
-      setPersons([...persons, { name: newName, number: newNumber }])
+      setPersons([...persons, { name: newName, number: newNumber, id: persons.length > 0 ? persons[persons.length-1].id + 1 : 1}])
       setNewName('')
       setNewNumber('')
     } else {
@@ -29,12 +34,18 @@ const App = () => {
       setNewName(value)
     } else if (name === 'number') {
       setNewNumber(value)
+    } else if (name === 'filter') {
+      setFilter(value)
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input name='filter' value={filter} onChange={handleChange} />
+      </div>
+      <h3>Add a new</h3>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input name='name' value={newName} onChange={handleChange} />
@@ -47,7 +58,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{`${person.name } ${person.number }`}</p>)}
+      {persons.filter(person=>person.name.toLowerCase().includes(filter.toLowerCase())).map(person => <p key={person.id}>{`${person.name } ${person.number }`}</p>)}
     </div>
   )
 }
